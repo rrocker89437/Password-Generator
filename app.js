46860653e9be07e1b08dafb466b253a9aa6d18bf
rrocker89437 <ramiah.rocker@yahoo.com>
2025-01-28 13:03:46 -0800

    Completed Project
    This was extremely hard but I'm happy I finished it

--- app.js
+++ app.js
@@ -1,103 +1,10 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
-
 // Copy Passwords
 const button1 = document.getElementById("copy1-el");
 const button2 = document.getElementById("copy2-el");
-
 const toCopy1 = document.getElementById("pass1-el");
 const toCopy2 = document.getElementById("pass2-el");
+
+const passwordGenerator = document.getElementById("genPass-el");
 
 let button1Clicked = false;
 let button2Clicked = false;
@@ -128,3 +35,39 @@ function copyToClipboard(text) {
       console.error("Failed to copy: ", err);
     });
 }
+
+//-------------------------------------------------------
+
+//Password Generator
+
+passwordGenerator.addEventListener("click", () => {
+  function genPass(len) {
+    var length = len ? len : 15;
+    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper
+    var numeric = "0123456789";
+    var punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var password = "";
+    var character = "";
+    var crunch = true;
+    while (password.length < length) {
+      entity1 = Math.ceil(string.length * Math.random() * Math.random());
+      entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
+      entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
+      hold = string.charAt(entity1);
+      hold = password.length % 2 == 0 ? hold.toUpperCase() : hold;
+      character += hold;
+      character += numeric.charAt(entity2);
+      character += punctuation.charAt(entity3);
+      password = character;
+    }
+    password = password
+      .split("")
+      .sort(function () {
+        return 0.5 - Math.random();
+      })
+      .join("");
+    return password.substr(0, len);
+  }
+  toCopy1.textContent = genPass();
+  toCopy2.textContent = genPass();
+});
